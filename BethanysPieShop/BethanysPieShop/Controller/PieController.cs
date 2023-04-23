7070e6c098c0f9a38ea793e4e943f2925f4943a4
Varsha Fernandes <varsha.fernandes@asos.com>
2023-04-23 01:39:04 +0100

    Models added

---
+++ BethanysPieShop/BethanysPieShop/Controller/PieController.cs
@@ -0,0 +1,27 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controller
+{
+    public class PieController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {
+           /* ViewBag.CurrentCategory = "Cheese cakes";
+            return View(_pieRepository.AllPies);*/
+           PieListViewModel piesListViewModel = new PieListViewModel(
+               _pieRepository.AllPies, "Cheese cakes");
+           return View(piesListViewModel);
+        }
+    }
+}
---
+++ BethanysPieShop/BethanysPieShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace BethanysPieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            {
+                new Category { CategoryId = 1, CategoryName = "Fruit Pies", Description = "All Fruity Pies"},
+                new Category { CategoryId = 2, CategoryName = "Cheese Cakes", Description = "Cheesy All The Way"},
+                new Category { CategoryId = 3, CategoryName = "Seasonal Pies", Description = "Get in mood for the seasonal pie"},
+            };
+    }
+}
